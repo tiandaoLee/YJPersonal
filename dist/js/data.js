bc68f9007b2357b18632e2151f87e1d5abece9d0
xiezhanggen@gmail.com <jaycie6461516>
2016-06-16 15:25:45 +0800

    测试

--- dist/js/data.js
+++ dist/js/data.js
@@ -129,6 +129,7 @@ var YJSay = (function ($) {
 		  });
 		},
 		scanQRCode:function(){
+			alert("点到了");
 			wx.scanQRCode({
 				needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
 				scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
