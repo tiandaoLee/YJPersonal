34c9f34cac60226edd8e4fee9824d32673b5ba6d
xiezhanggen@gmail.com <jaycie6461516>
2016-06-16 16:12:48 +0800

    测试

--- dist/js/data.js
+++ dist/js/data.js
@@ -7,7 +7,7 @@ var YJSay = (function ($) {
     var securityKey="";
     var alarmList="";
     var addSaleStatus;
-	var weixin = {appId:"wx93ccd8dccdd06205",nonceStr:"Iup3I2PGsq09cSpz",timestamp:new Date().getTime()/1000};
+	var weixin = {appId:"wx2ce5de42f31757ea",nonceStr:"Iup3I2PGsq09cSpz",timestamp:new Date().getTime()/1000};
     //var weixin = {appId:"wx93ccd8dccdd06205",nonceStr:"V79ub0LNlSzUqiEs",timestamp:1466063208};
 	return {
         baseUrl:"http://src.yjsvip.com",
